04ef4237226f84dd3d235376cfd4774c6c8e498d
Esteban Lemus <esteban.lemus@javeriana.edu.co>
2019-03-17 16:40:15 -0500

    creacion carpeta

---
+++ Taller 6/src/spController.java
@@ -0,0 +1,56 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class spController
+ */
+@WebServlet("/spController")
+public class spController extends HttpServlet 
+{
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public spController() 
+    {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		// TODO Auto-generated method stub
+		
+		String opcion = request.getParameter("opcion");
+		
+		if (opcion.equals("Login"))
+		{
+			response.sendRedirect("./spLogin");
+		}
+		if (opcion.equals("ChangePassword"))
+		{
+			response.sendRedirect("./spChangePassword");
+		}
+			
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
